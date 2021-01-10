4bc322424685bdf64ae2d242e68fff7e0d40275d
yusuf963 <mohammad963yusuf@gmail.com>
2021-01-10 19:10:51 +0000

    creating array for nav items and looping over using forEach

--- script.js
+++ script.js
@@ -5,17 +5,46 @@ const nav2 = document.getElementById("nav-2")
 const nav3 = document.getElementById("nav-3")
 const nav4 = document.getElementById("nav-4")
 const nav5 = document.getElementById("nav-5")
+const navItems = ['nav1', 'nav2', 'nav3', 'nav4', 'nav5']
 
+
+//control navigation anmation for each nav item
+// function navAnimation (direction1, direction2){
+//   navItems.forEach((nav, i) => {
+//     nav.classList.replace(`slide-${direction1}-${i + 1}`, `slide-${direction2}-${i + 1}`)
+//   })
+// }
 //creating function 
 const toggleNav = ()=>{
   menuBars.classList.toggle('change')
   overlay.classList.toggle('overlay-active')
   if (overlay.classList.contains('overlay-active')){
-    overlay.classList.remove('overlay-slide-left')
-    overlay.classList.add('overlay-slide-right')
+    overlay.classList.replace('overlay-slide-left', 'overlay-slide-right')
+    // animate-in nav items
+    nav1.classList.remove('slide-out-1')
+    nav1.classList.add('slide-in-1')
+    nav2.classList.remove('slide-out-2')
+    nav2.classList.add('slide-in-2')
+    nav3.classList.remove('slide-out-3')
+    nav3.classList.add('slide-in-3')
+    nav4.classList.remove('slide-out-4')
+    nav4.classList.add('slide-in-4')
+    nav5.classList.remove('slide-out-5')
+    nav5.classList.add('slide-in-5')
+    
   } else {
-    overlay.classList.remove('overlay-slide-right')
-    overlay.classList.add('overlay-slide-left')
+    overlay.classList.replace('overlay-slide-right', 'overlay-slide-left')
+    //animate out nav items
+    nav1.classList.remove('slide-in-1')
+    nav1.classList.add('slide-out-1')
+    nav2.classList.remove('slide-in-2')
+    nav2.classList.add('slide-out-2')
+    nav3.classList.remove('slide-in-3')
+    nav3.classList.add('slide-out-3')
+    nav4.classList.remove('slide-in-4')
+    nav4.classList.add('slide-out-4')
+    nav5.classList.remove('slide-in-5')
+    nav5.classList.add('slide-out-5')
   }
 }
 
